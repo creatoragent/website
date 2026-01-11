ebbd2b6c52e770aa79c81c132202e6e72b2ff06c
creatoragent <hello@visualsofearth.com>
2026-01-11 01:26:45 -0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Test from './Test'
 import Agent from './Agent'
 import Login from './Login'
 import Checkout from './Checkout'
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/agent" element={<Agent />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/test" element={<Test />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
